729f6bb89d4e90d8157d1b6ae9b8048c974fcdf8
soto <victor.alonso@xmarts.com>
2019-03-07 15:40:40 -0600

    cambios al modelo
    se le duplica el ticket y el reporte de ventas p

---
+++ pos_add_hf/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "POS Encabezado/Pie de pagina V2",
+
+    'summary': "Para odoo12",
+
+    'description': "Agrega un encabezado y un pie de pagina al ticket del POS",
+
+    'author': "Pablo Osorio",
+
+    # Categories can be used to filter modules in modules listing
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','point_of_sale','sale'],
+
+    # always loaded
+    'data': [
+        'static/src/xml/assets.xml',
+        'views/views.xml',
+        'reports/report_recibo_ticket.xml',
+        'reports/reports_menu.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'qweb': [
+        'static/src/xml/pos.xml'
+    ],
+}
